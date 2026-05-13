dbf0f028149e08026d57a6edd021cdc3ffe0a7bc
jhulmar98 <jhulmarsoftware@gmail.com>
2026-05-13 11:36:47 -0500

    Update server.js

--- BACK END/server.js
+++ BACK END/server.js
@@ -10,7 +10,14 @@ const pool = require("./db");
 const app = express();
 const server = http.createServer(app);
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+  limit: "50mb"
+}));
+
+app.use(express.urlencoded({
+  extended: true,
+  limit: "50mb"
+}));
 app.use(webRoutes);
 
 const PORT = process.env.PORT || 3000;
